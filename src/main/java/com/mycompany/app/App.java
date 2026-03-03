96e0fc87fa61ac00d337dcc190de86bc1c02a610
Marcus Franklin <marcus.franklin.j12@gmail.com>
2026-03-03 21:48:21 +0530

    test 2

--- src/main/java/com/mycompany/app/App.java
+++ src/main/java/com/mycompany/app/App.java
@@ -5,7 +5,7 @@ package com.mycompany.app;
  */
 public class App {
 
-    private static final String MESSAGE = "Hello mogger";
+    private static final String MESSAGE = "Hello Franklin";
 
     public App() {}
 
